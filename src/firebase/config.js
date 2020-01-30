a1a722d0c66d174de35fb75a1eab9e1f18b5efc9
ch-shubham <shubham128166@gmail.com>
2020-01-30 15:26:12 +0530

    Updates on firebase project

---
+++ src/firebase/config.js
@@ -0,0 +1,8 @@
+export const config = {
+  apiKey: "AIzaSyDVPm5MZFlOi1QNn5pcrgcZm2fjhaI0J2M",
+  authDomain: "confusionserver-123.firebaseapp.com",
+  databaseURL: "https://confusionserver-123.firebaseio.com",
+  projectId: "confusionserver-123",
+  storageBucket: "confusionserver-123.appspot.com",
+  messagingSenderId: "1044279517116"
+};
